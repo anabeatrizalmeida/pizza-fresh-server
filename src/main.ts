527224a7242619b00b7cbeefa848872bad7707d9
Ana Beatriz de Almeida <97922588+anabeatrizalmeida@users.noreply.github.com>
2022-07-07 17:43:43 -0300

    feat(product): implement controller

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -14,12 +14,13 @@ async function bootstrap() {
     .setVersion('1.0.0')
     .addTag('status')
     .addTag('table')
+    .addTag('product')
     .build();
 
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
-
   await app.listen(3333);
 }
+
 bootstrap();
--- src/product/product.controller.ts
+++ src/product/product.controller.ts
@@ -1,34 +1,62 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  HttpCode,
+  HttpStatus,
+} from '@nestjs/common';
 import { ProductService } from './product.service';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
 
+@ApiTags('product')
 @Controller('product')
 export class ProductController {
   constructor(private readonly productService: ProductService) {}
 
   @Post()
+  @ApiOperation({
+    summary: 'Create a product',
+  })
   create(@Body() createProductDto: CreateProductDto) {
     return this.productService.create(createProductDto);
   }
 
   @Get()
+  @ApiOperation({
+    summary: 'List all products',
+  })
   findAll() {
     return this.productService.findAll();
   }
 
   @Get(':id')
+  @ApiOperation({
+    summary: 'View a product by ID',
+  })
   findOne(@Param('id') id: string) {
-    return this.productService.findOne(+id);
+    return this.productService.findOne(id);
   }
 
   @Patch(':id')
+  @ApiOperation({
+    summary: 'Edit a product by ID',
+  })
   update(@Param('id') id: string, @Body() updateProductDto: UpdateProductDto) {
-    return this.productService.update(+id, updateProductDto);
+    return this.productService.update(id, updateProductDto);
   }
 
   @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.productService.remove(+id);
+  @HttpCode(HttpStatus.NO_CONTENT)
+  @ApiOperation({
+    summary: 'Remove a product by ID',
+  })
+  delete(@Param('id') id: string) {
+    this.productService.delete(id);
   }
 }
