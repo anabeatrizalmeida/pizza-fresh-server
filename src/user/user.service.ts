ff09780b00e0eb2a78039ffac8839f2c85bb4e9d
Ana Beatriz de Almeida <97922588+anabeatrizalmeida@users.noreply.github.com>
2022-07-11 15:52:19 -0300

    feat(user): remove `password` from `UserService`serialization

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,56 +1,102 @@
-import { Injectable, NotFoundException, UnprocessableEntityException } from '@nestjs/common';
-import { CreateUserDto } from './dto/create-user.dto';
-import { User } from './entities/user.entity';
+import {
+  BadRequestException,
+  Injectable,
+  NotFoundException,
+  UnprocessableEntityException,
+} from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { User } from './entities/user.entity';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class UserService {
-  async delete(id: string) {
-    await this.findById(id);
+  private userSelect = {
+    id: true,
+    nickname: true,
+    name: true,
+    password: false,
+    image: true,
+    createdAt: true,
+    updatedAt: true,
+  };
 
-    await this.prisma.user .delete({ where: { id } })
-  }
-  async update(id: string, createUserDto: UpdateUserDto): Promise<User > {
-    await this.findById(id);
-
-    delete createUserDto.confirmPassword;
-
-    const data: Partial<User > = { ...createUserDto};
-
-    return this.prisma.user .update({
-      where: { id },
-      data,
-    }).catch(this.handleError);
-  }
   constructor(private readonly prisma: PrismaService) {}
 
-  findAll(): Promise<User []> {
-    return this.prisma.user .findMany();
+  findAll(): Promise<User[]> {
+    return this.prisma.user.findMany({
+      select: this.userSelect,
+    });
   }
 
-  async findById(id: string): Promise<User >{
-    const record = await this.prisma.user .findUnique({ where: { id } });
+  async findById(id: string): Promise<User> {
+    const record = await this.prisma.user.findUnique({
+      where: { id },
+      select: this.userSelect,
+    });
 
     if (!record) {
-      throw new NotFoundException(`Record with '${id}' not found.`)
+      throw new NotFoundException(`Record with ID '${id}' not found.`);
     }
 
     return record;
   }
 
-  async findOne(id: string): Promise<User > {
+  async findOne(id: string): Promise<User> {
     return this.findById(id);
   }
 
-  create(createUserDto: CreateUserDto): Promise<User > {
-    delete createUserDto.confirmPassword;
+  async create(dto: CreateUserDto): Promise<User> {
+    if (dto.password != dto.confirmPassword) {
+      throw new BadRequestException('The passwords provided are not the same.');
+    }
 
-    const user : User  = {...createUserDto };
+    delete dto.confirmPassword;
 
-    return this.prisma.user .create({
-      data: user ,
-    }).catch(this.handleError);
+    const data: User = {
+      ...dto,
+      password: await bcrypt.hash(dto.password, 10),
+    };
+
+    return this.prisma.user
+      .create({
+        data,
+        select: this.userSelect,
+      })
+      .catch(this.handleError);
+  }
+
+  async update(id: string, dto: UpdateUserDto): Promise<User> {
+    await this.findById(id);
+
+    if (dto.password) {
+      if (dto.password != dto.confirmPassword) {
+        throw new BadRequestException('The passwords provided are not the same.');
+      }
+    }
+
+    delete dto.confirmPassword;
+
+    const data: Partial<User> = { ...dto };
+
+    if (data.password) {
+      data.password = await bcrypt.hash(data.password, 10);
+    }
+
+    return this.prisma.user
+      .update({
+        where: { id },
+        data,
+        select: this.userSelect,
+      })
+      .catch(this.handleError);
+  }
+
+  async delete(id: string) {
+    await this.findById(id);
+
+    await this.prisma.user.delete({ where: { id } });
   }
 
   handleError(error: Error): undefined {
@@ -66,4 +112,3 @@ export class UserService {
     );
   }
 }
-
